066d577b3fc4a3929d9d4d40dbd6abb6f5e9b892
morrjw <1060236395@qq.com>
2024-06-28 18:21:33 +0800

    1111

--- .vitepress/config.mjs
+++ .vitepress/config.mjs
@@ -1,5 +1,5 @@
 import { defineConfig } from 'vitepress'
-// import { set_sidebar } from "../utils/auto-gen-sidebar.mjs"
+// import { set_sidebar } from "./components/auto-gen-sidebar.mjs"
 
 // https://vitepress.dev/reference/site-config
 export default defineConfig({
@@ -21,26 +21,25 @@ export default defineConfig({
       provider: 'local',
       options: {
         locales: {
-          zh: { // make this `root` if you want to translate the default locale
-            translations: {
-              button: {
-                buttonText: '搜索',
-                buttonAriaLabel: '搜索'
-              },
-              modal: {
-                displayDetails: '显示详细列表',
-                resetButtonTitle: '重置搜索',
-                backButtonTitle: '关闭搜索',
-                noResultsText: '没有结果',
-                footer: {
-                  selectText: '选择',
-                  selectKeyAriaLabel: '输入',
-                  navigateText: '导航',
-                  navigateUpKeyAriaLabel: '上箭头',
-                  navigateDownKeyAriaLabel: '下箭头',
-                  closeText: '关闭',
-                  closeKeyAriaLabel: 'esc'
-                }
+          // make this `root` if you want to translate the default locale
+          translations: {
+            button: {
+              buttonText: '搜索',
+              buttonAriaLabel: '搜索'
+            },
+            modal: {
+              displayDetails: '显示详细列表',
+              resetButtonTitle: '重置搜索',
+              backButtonTitle: '关闭搜索',
+              noResultsText: '没有结果',
+              footer: {
+                selectText: '选择',
+                selectKeyAriaLabel: '输入',
+                navigateText: '导航',
+                navigateUpKeyAriaLabel: '上箭头',
+                navigateDownKeyAriaLabel: '下箭头',
+                closeText: '关闭',
+                closeKeyAriaLabel: 'esc'
               }
             }
           }
