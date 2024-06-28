89eaa50c82927eb3a3a1c8301886ace956d30fa5
morrjw <1060236395@qq.com>
2024-06-28 14:23:06 +0800

    1

---
+++ .vitepress/config.mjs
@@ -0,0 +1,60 @@
+import { defineConfig } from 'vitepress'
+// import { set_sidebar } from "../utils/auto-gen-sidebar.mjs"
+
+// https://vitepress.dev/reference/site-config
+export default defineConfig({
+  base: '/note/',
+  title: "印象笔记",
+  description: "一个vitepress文档",
+  head: [['link', { rel: 'icon', href: '/logo.png' }]],
+  themeConfig: {
+    search: {
+      provider: 'local',
+      options: {
+        locales: {
+          zh: { // make this `root` if you want to translate the default locale
+            translations: {
+              button: {
+                buttonText: '搜索',
+                buttonAriaLabel: '搜索'
+              },
+              modal: {
+                displayDetails: '显示详细列表',
+                resetButtonTitle: '重置搜索',
+                backButtonTitle: '关闭搜索',
+                noResultsText: '没有结果',
+                footer: {
+                  selectText: '选择',
+                  selectKeyAriaLabel: '输入',
+                  navigateText: '导航',
+                  navigateUpKeyAriaLabel: '上箭头',
+                  navigateDownKeyAriaLabel: '下箭头',
+                  closeText: '关闭',
+                  closeKeyAriaLabel: 'esc'
+                }
+              }
+            }
+          }
+        }
+      }
+    },
+    outlineTitle: "文章目录",
+    outline: [2, 6],
+    sidebar: false, // 关闭侧边栏
+    aside: "left", // 设置右侧侧边栏在左侧显示
+    logo: "logo.png",
+    // https://vitepress.dev/reference/default-theme-config
+    nav: [
+      { text: '主页', link: '/' },
+      { text: '示例', link: '/markdown-examples' }
+    ],
+    // sidebar:{"/front": set_sidebar("front")},
+    socialLinks: [
+      { icon: 'github', link: 'https://github.com/vuejs/vitepress' }
+    ],
+    footer: {
+      message: 'Released under the Morrow License.',
+      copyright: 'Copyright © 2024-present Evan You'
+    }
+  }
+})
---
+++ index.md
@@ -0,0 +1,27 @@
+---
+# https://vitepress.dev/reference/default-theme-home-page
+layout: home
+
+hero:
+  name: "印象笔记"
+  text: "一个vitepress文档"
+  tagline: Day Day Up
+  image: 
+    src: "/backend.png"
+    alt: 背景图
+  actions:
+    - theme: brand
+      text: Markdown
+      link: /markdown-examples
+    - theme: alt
+      text: API
+      link: /api-examples
+
+features:
+  - title: 性能优势
+    details: 建立在 Vite 上意味着可以直接从其丰富的生态系统中利用 Vite 插件。这使得 VitePress 成为构建以内容为中心但也需要非平凡交互性的网站的理想选择
+  - title: 易用性
+    details: VitePress 提供了一个功能丰富的默认主题，专为文档目的而设计。这使得用户可以以最小的努力启动漂亮的文档站点，无需任何特定于 Vue 的知识
+  - title: 生态系统
+    details: 建立在 Vite 上意味着可以直接从其丰富的生态系统中利用 Vite 插件。这使得 VitePress 成为构建以内容为中心但也需要非平凡交互性的网站的理想选择。
+---
