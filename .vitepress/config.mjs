8f96a18457ecae11a3395a74bb450530c3e56bf2
morrjw <1060236395@qq.com>
2024-07-01 11:26:17 +0800

    配置

--- .vitepress/config.mjs
+++ .vitepress/config.mjs
@@ -1,9 +1,10 @@
 import { defineConfig } from 'vitepress'
-// import { set_sidebar } from "./components/auto-gen-sidebar.mjs"
+import { set_sidebar } from "./components/auto-gen-sidebar.mjs"
 
 // https://vitepress.dev/reference/site-config
 export default defineConfig({
   base: '/note/',
+  lang: "zh",
   title: "知识库",
   description: "一个vitepress文档",
   head: [['link', { rel: 'icon', href: '/logo.png' }]],
@@ -17,30 +18,36 @@ export default defineConfig({
     }
   },
   themeConfig: {
+    // 更新时间的显示
+    lastUpdated: {
+      text: '更新于',
+      formatOptions: {
+        dateStyle: 'medium',
+        timeStyle: 'medium'
+      }
+    },
+    //搜索
     search: {
       provider: 'local',
       options: {
-        locales: {
-          // make this `root` if you want to translate the default locale
-          translations: {
-            button: {
-              buttonText: '搜索',
-              buttonAriaLabel: '搜索'
-            },
-            modal: {
-              displayDetails: '显示详细列表',
-              resetButtonTitle: '重置搜索',
-              backButtonTitle: '关闭搜索',
-              noResultsText: '没有结果',
-              footer: {
-                selectText: '选择',
-                selectKeyAriaLabel: '输入',
-                navigateText: '导航',
-                navigateUpKeyAriaLabel: '上箭头',
-                navigateDownKeyAriaLabel: '下箭头',
-                closeText: '关闭',
-                closeKeyAriaLabel: 'esc'
-              }
+        translations: {
+          button: {
+            buttonText: '搜索',
+            buttonAriaLabel: '搜索'
+          },
+          modal: {
+            displayDetails: '显示详细列表',
+            resetButtonTitle: '重置搜索',
+            backButtonTitle: '关闭搜索',
+            noResultsText: '没有结果',
+            footer: {
+              selectText: '选择',
+              selectKeyAriaLabel: '输入',
+              navigateText: '导航',
+              navigateUpKeyAriaLabel: '上箭头',
+              navigateDownKeyAriaLabel: '下箭头',
+              closeText: '关闭',
+              closeKeyAriaLabel: 'esc'
             }
           }
         }
@@ -54,15 +61,21 @@ export default defineConfig({
     // https://vitepress.dev/reference/default-theme-config
     nav: [
       { text: '主页', link: '/' },
+      { text: '前端', link: '/docs/web' },
       { text: 'markdown', link: '/docs/markdown' }
     ],
-    // sidebar:{"/front": set_sidebar("front")},
+    //sidebar: { "/front": set_sidebar("/docs") },
     socialLinks: [
       { icon: 'github', link: 'https://github.com/vuejs/vitepress' }
     ],
     footer: {
       message: 'Released under the Morrow License.',
       copyright: 'Copyright © 2024-present Evan You'
-    }
+    },
+    notFound: { // 404页
+      title: '未找到页面，迷路了~',
+      quote: '请检查地址是否正确，或当前页面未开通，点击下方按钮返回首页',
+      linkText: '返回首页'
+    },
   }
 })
